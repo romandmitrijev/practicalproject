c68a2ff7694be8dcfed6723aff556eae7eb75de5
Dmitri Styf <Dmitri.styf@gmail.com>
2019-06-11 19:46:44 +0300

    start of project

---
+++ src/main/java/com/sda/tallinn4/practicalproject/PracticalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sda.tallinn4.practicalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/tallinn4/practicalproject/PracticalprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sda.tallinn4.practicalproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticalprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
