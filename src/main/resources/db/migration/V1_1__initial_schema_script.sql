c97dfedce08678165eddf2aaf1203343671de8f7
Roman Dmitrijev <roman@dmitrijev.net>
2019-06-12 20:32:38 +0300

    init commit

---
+++ src/main/resources/db/migration/V1_1__initial_schema_script.sql
@@ -0,0 +1,65 @@
+USE sda_final_project;
+
+CREATE TABLE user (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(15) NOT NULL,
+    password VARCHAR(15) NOT NULL,
+    usertype VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE userdetails (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    firstname VARCHAR(15) NOT NULL,
+    lastname VARCHAR(15),
+    email VARCHAR(20),
+    phonenumber INT,
+    country VARCHAR(20) NOT NULL,
+    city VARCHAR(20)
+);
+
+
+CREATE TABLE company(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+	companyname VARCHAR(20),
+    reg_number VARCHAR(20),
+    postindex VARCHAR(20)
+);
+
+CREATE TABLE country(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    city_id INT NOT NULL,
+	countryname VARCHAR(20)
+);
+
+CREATE TABLE city(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+	cityname VARCHAR(20)
+);
+
+CREATE TABLE cargo(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+	cargo_description VARCHAR(50),
+    cargoweight INT,
+    cargo_size VARCHAR(20)
+);
+
+CREATE TABLE vehicle(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id LONG NOT NULL,
+	capacity INT(20),
+    loading_place VARCHAR(20),
+    delivery_place VARCHAR(20)
+);
+
+CREATE TABLE shipment(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id LONG NOT NULL,
+	cargo_id LONG NOT NULL,
+    shipment_id LONG NOT NULL,
+    comment VARCHAR(20)
+);
