f8f77183c5f3a08a7ef238237feb679e4abc1dae
Dmitri Styf <Dmitri.styf@gmail.com>
2019-06-19 19:20:33 +0300

    vehicle search and cargo search ready

--- src/main/java/com/sda/tallinn4/practicalproject/model/Vehicle.java
+++ src/main/java/com/sda/tallinn4/practicalproject/model/Vehicle.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Vehicle {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//
     private Long id;
 
     @ManyToOne
